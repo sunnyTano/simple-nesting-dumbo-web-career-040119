e96e2bb601bd5cc8f9baefaec8b5b5d278a69648
Susan Tan <ssusantan9@gmail.com>
2019-03-26 05:01:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,11 +85,11 @@ def adding_matz
           :languages => ["C"]
         }
     }
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# 
+add the following information to the top level of programmer_hash
+:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+
     
 end
 
